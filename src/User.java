0e60e3cc5d7f47f87eac6bb3697e76965fcdaf3a
ArtemK88 <kapiton2007@gmail.com>
2023-06-25 21:48:48 +0300

    Initial commit

---
+++ src/User.java
@@ -0,0 +1,33 @@
+public class Users {
+    private String firstName;
+    private String secondName;
+    private int age;
+
+    public Users(String firstName, String secondName, int age) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
